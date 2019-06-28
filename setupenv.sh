ff3dec39d84f1c64ea22d9d328bd29d76d6d504b
wmsegar <wayne.segar@gmail.com>
2019-06-28 15:44:10 -0400

    added rm for cr.yaml

--- setupenv.sh
+++ setupenv.sh
@@ -15,6 +15,8 @@ kubectl create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent
 
 kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken="$1 --from-literal="paasToken="$2
 
+rm cr.yaml
+
 curl -o cr.yaml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/cr.yaml
 
 sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$3'.dynatrace-managed.com\/e\/'$4'\/api/' cr.yaml
@@ -30,5 +32,3 @@ echo "Deploying SockShop Application"
 ./deploy-sockshop.sh
 
 echo "Deployment Complete"
-
-
