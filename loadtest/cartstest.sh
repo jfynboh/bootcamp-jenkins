e2e16ad56d9d092efacfde8f81cec31a95949f9b
wmsegar <wayne.segar@gmail.com>
2019-07-17 15:04:07 -0400

    uncomment

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,4 +1,4 @@
 echo 'Hello World!'
 
-#export CARTS_URL=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
-#echo $CARTS_URL
+export CARTS_URL=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
+echo $CARTS_URL
