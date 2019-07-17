0e7b1fba2f75902a96e48d47a351086dd97e1f8a
wmsegar <wayne.segar@gmail.com>
2019-07-17 17:33:11 -0400

    added new loadtest file

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,5 +1,2 @@
 export CARTS_IP=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
 export CARTS_URL="http://"$CARTS_IP":8080/cart"
-echo $CARTS_URL
-
-#curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "$CARTS_URL"
---
+++ loadtest/loadtest.sh
@@ -0,0 +1,1 @@
+curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "$CARTS_URL"
