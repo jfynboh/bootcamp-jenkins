0c8273dd188bc84c41a31809b789f11a5903c7bc
wmsegar <wayne.segar@gmail.com>
2019-07-17 17:35:35 -0400

    hard coded URL

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,1 +1,1 @@
-curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "$CARTS_URL"
+curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "http://35.225.231.81:8080/cart"
