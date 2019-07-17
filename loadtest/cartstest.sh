4b30489d8f0b85c6fac01af98e8f71c4551cec6b
wmsegar <wayne.segar@gmail.com>
2019-07-17 16:53:32 -0400

    updated cartstest

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -2,4 +2,4 @@ export CARTS_IP=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress
 export CARTS_URL="http://"$CARTS_IP":8080/cart"
 echo $CARTS_URL
 
-curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "$CARTS_URL"
+#curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST "$CARTS_URL"
