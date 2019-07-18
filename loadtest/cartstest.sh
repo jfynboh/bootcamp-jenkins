a3114ae781d0d676a9c748bafbaf419fd06100d4
wmsegar <wayne.segar@gmail.com>
2019-07-18 14:57:42 -0400

    updated cartstest and vuser count

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,4 +1,4 @@
-export CARTS_IP=$(kubectl describe svc front-end -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
-export CARTS_URL="http:\/\/"$CARTS_IP":8080\/cart"
+export CARTS_IP=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
+#export CARTS_URL="http:\/\/"$CARTS_IP":8080\/cart"
 
-sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_URL/g" loadtest.sh
+sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_IP/g" carts_load.jmx
