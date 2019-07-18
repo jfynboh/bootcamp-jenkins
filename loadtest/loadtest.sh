b0d57fd278308d7c7f82b092eb9dd8fd790558b5
wmsegar <wayne.segar@gmail.com>
2019-07-18 10:58:28 -0400

    changed to iterations

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,7 +1,10 @@
 echo "Load Test Starting for 5 min..."
 
-while [ (($SECONDS < 30)) ]; do
+export ITER=0
+
+while [ $ITER -le 30 ]; do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+    ((ITER++))
 done
 
 echo "Load Test Completed"
