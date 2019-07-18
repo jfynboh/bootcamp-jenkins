1756757217f792fec34302c85fd2a2c0e254554b
wmsegar <wayne.segar@gmail.com>
2019-07-18 11:14:39 -0400

    changed iteration

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,10 +1,11 @@
 echo "Load Test Starting for 5 min..."
 
-export ITER=0
+iter=0
 
-while [ $ITER -le 30 ]; do
+while [ $iter -le 30 ]; do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
-    ((ITER++))
+    ((iter+=1))
+    echo $iter
 done
 
 echo "Load Test Completed"
