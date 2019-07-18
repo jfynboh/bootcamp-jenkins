bc4b25aa369dd0ce1c4ee67afb7bedd39042fd5f
wmsegar <wayne.segar@gmail.com>
2019-07-18 08:56:47 -0400

    updated loadtest files

--- loadtest/cartstest.sh
+++ loadtest/cartstest.sh
@@ -1,2 +1,4 @@
-export CARTS_IP=$(kubectl describe svc carts -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
-export CARTS_URL="http://"$CARTS_IP":8080/cart"
+export CARTS_IP=$(kubectl describe svc front-end -n dev | grep 'LoadBalancer Ingress:' | sed 's~LoadBalancer Ingress:[ \t]*~~')
+export CARTS_URL="http:\/\/"$CARTS_IP":8080\/cart"
+
+sed -i "s/CARTS_URL_PLACEHOLDER/$CARTS_URL/g" loadtest.sh
