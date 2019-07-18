f9a0cfe8311988d3cb1cd0cae259893fc386aa79
wmsegar <wayne.segar@gmail.com>
2019-07-18 12:17:14 -0400

    added 2s sleep

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -2,9 +2,16 @@ echo "Load Test Starting for 5 min..."
 
 i=0
 
-while [ $i -le 30 ]
+while [ $i -le 25 ]
 do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+
+    sleep 2
+
     i=$(($i + 1))
 done
 
