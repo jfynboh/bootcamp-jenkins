7a7ce573ddf288d56983ab3d75b213dd0c946842
wmsegar <wayne.segar@gmail.com>
2019-07-18 11:23:15 -0400

    removed loop

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,11 +1,5 @@
 echo "Load Test Starting for 5 min..."
 
-iter=0
-
-while [ $iter -le 30 ]; do
-    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
-    ((iter+=1))
-    echo $iter
-done
+curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
 
 echo "Load Test Completed"
