bf13c646a7e9067b66d0e1a2a0d6ff38da8de9a9
wmsegar <wayne.segar@gmail.com>
2019-07-18 10:53:11 -0400

    brackets in while loop

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,6 +1,6 @@
 echo "Load Test Starting for 5 min..."
 
-while (($SECONDS < 30)); do
+while [ (($SECONDS < 30)) ]; do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
 done
 
