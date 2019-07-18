39c385e89a3b802a64f81d141a0eeea4953c0f39
wmsegar <wayne.segar@gmail.com>
2019-07-18 12:20:45 -0400

    updated test to run for 5 min

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -2,7 +2,7 @@ echo "Load Test Starting for 5 min..."
 
 i=0
 
-while [ $i -le 25 ]
+while [ $i -le 125 ]
 do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
