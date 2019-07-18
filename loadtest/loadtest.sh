a339f015af1a6a98371f5a6550a2efa2b6ca52e7
wmsegar <wayne.segar@gmail.com>
2019-07-18 12:07:36 -0400

    updated script to work with sh

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -5,7 +5,7 @@ i=0
 while [ $i -le 30 ]
 do
     curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
-    ((i = $i+1))
+    i=$(($i + 1))
 done
 
 echo "Load Test Completed"
