cdf4b9118d2af3af0e6e0dc66e6611bb2f4aa912
wmsegar <wayne.segar@gmail.com>
2019-07-18 10:50:06 -0400

    changed while loop

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,8 +1,7 @@
 echo "Load Test Starting for 5 min..."
 
-end=$((SECONDS+30))
-while [ $SECONDS -lt $end ]; do
-    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST http://35.188.92.227:8080/cart
+while (($SECONDS < 30)); do
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
 done
 
 echo "Load Test Completed"
