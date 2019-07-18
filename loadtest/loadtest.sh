afff2532e0417bf59b3a1db302fee1dfecb449a5
wmsegar <wayne.segar@gmail.com>
2019-07-18 10:40:29 -0400

    added loadtest time to script

--- loadtest/loadtest.sh
+++ loadtest/loadtest.sh
@@ -1,1 +1,8 @@
-curl -d '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST CARTS_URL_PLACEHOLDER
+echo "Load Test Starting for 5 min..."
+
+end=$((SECONDS+30))
+while [ $SECONDS -lt $end ]; do
+    curl -o /dev/null -s -w "%{http_code}\n" -d  '{"id":"510a0d7e-8e83-4193-b483-e27e09ddc34d"}' -H "Content-Type: application/json" -X POST http://35.188.92.227:8080/cart
+done
+
+echo "Load Test Completed"
